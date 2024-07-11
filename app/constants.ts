9bc703059d9311cb76ff4df71917e639a02d8510
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-12 01:40:57 +0530

    Moved meta data into constants file

--- app/constants.ts
+++ app/constants.ts
@@ -1,7 +1,50 @@
 import { ThemeType } from "@/types";
+import { ReferrerEnum } from "next/dist/lib/metadata/types/metadata-types";
 
 // Brand constants
 const BRAND_NAME = "Knowtion";
+
+// Metadata constanst
+const META = {
+  title: `${BRAND_NAME}`,
+  desciption:
+    "Create workspaces for your interacvtive teams and collaborate seamlessly",
+  icon: {
+    dark: {
+      media: "(prefers-color-scheme: dark)",
+      url: "/logo-dark.png",
+      href: "/logo-dark.png",
+    },
+    light: {
+      media: "(prefers-color-scheme: light)",
+      url: "/logo.png",
+      href: "/logo.png",
+    },
+  },
+  referrer: "origin-when-cross-origin" as ReferrerEnum,
+  keywords: [
+    "Next.js",
+    "React",
+    "JavaScript",
+    "Notion",
+    "Knowtion",
+    "Documents",
+  ],
+  authors: [
+    {
+      name: "Pratham Shinde",
+      url: "https://github.com/SoloProgrammer",
+    },
+  ],
+  url: "knowtion.vercel.app",
+  siteName: "Knowtion",
+  og: {
+    url: "/og.png", // Must be an absolute URL
+    width: 800,
+    height: 600,
+  },
+  locale: "en_US",
+};
 
 // Theme constants
 const DARK = "dark";
@@ -22,4 +65,4 @@ const THEMS: ThemeType[] = [
   },
 ];
 
-export { BRAND_NAME, THEMS, DARK, LIGHT, SYSTEM };
+export { META, BRAND_NAME, THEMS, DARK, LIGHT, SYSTEM };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,58 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { BRAND_NAME } from "./constants";
+import { META as meta } from "./constants";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `${BRAND_NAME}`,
-  description:
-    "Create workspaces for your interacvtive teams and collaborate seamlessly",
+  title: meta.title,
+  description: meta.desciption,
   icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "/logo.png",
-        href: "/logo.png",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/logo-dark.png",
-        href: "/logo-dark.png",
-      },
-    ],
+    icon: [meta.icon.dark, meta.icon.light],
   },
-  referrer: "origin-when-cross-origin",
-  keywords: [
-    "Next.js",
-    "React",
-    "JavaScript",
-    "Notion",
-    "Knowtion",
-    "Documents",
-  ],
-  authors: [
-    {
-      name: "Pratham Shinde",
-      url: "https://github.com/SoloProgrammer",
-    },
-  ],
+  referrer: meta.referrer,
+  keywords: meta.keywords,
+  authors: meta.authors,
   openGraph: {
-    title: `${BRAND_NAME}`,
-    description:
-      "Create workspaces for your interacvtive teams and collaborate seamlessly",
-    url: "knowtion.vercel.app",
-    siteName: "Knowtion",
-    images: [
-      {
-        url: "/og.png", // Must be an absolute URL
-        width: 800,
-        height: 600,
-      },
-    ],
-    locale: "en_US",
+    title: meta.title,
+    description: meta.desciption,
+    url: meta.url,
+    siteName: meta.siteName,
+    images: [meta.og],
+    locale: meta.locale,
     type: "website",
   },
 };
