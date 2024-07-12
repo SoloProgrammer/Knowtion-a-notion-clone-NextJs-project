37a4ad908fb6e4b3beef0d34a6461a8d3d9eb7eb
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-12 18:28:55 +0530

    Implemented auhtentication and created resuable spinner component

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -4,25 +4,51 @@ import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { Button } from "@/components/ui/button";
-import { LogIn } from "lucide-react";
+import { FileInput } from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import { BRAND_NAME } from "@/app/constants";
+import Link from "next/link";
 
 export const Navbar = () => {
   const isScrolled = useScrollTop();
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div
       className={cn(
-        "p-6 py-4 fixed top-0 left-0 w-full transition-colors flex justify-between items-center",
+        "px-3 md:px-6 py-4 fixed top-0 left-0 w-full transition-colors flex justify-between items-center",
         isScrolled && "shadow-sm bg-white dark:bg-neutral-900 border-b"
       )}
     >
       <Logo className="[&>div+p]:hidden md:[&>div+p]:block flex" />
       <div className="flex items-center gap-2">
         <ModeToggle />
-        <Button variant={"ghost"} size={"sm"} className="font-medium border">
-          <span className="hidden md:block">Login </span>
-          <LogIn className="w-4 h-4 ml-0 md:ml-2" />
-        </Button>
+        <div className="flex items-center gap-2">
+          {isLoading ? (
+            <Spinner />
+          ) : !isAuthenticated && !isLoading ? (
+            <SignInButton mode="modal">
+              <Button variant={"default"} size="sm" className="text-xs">
+                Get knowtion free
+              </Button>
+            </SignInButton>
+          ) : (
+            <div className="flex items-center gap-2">
+              <Link href={"/documents"} className="">
+                <Button variant={"ghost"} size={"sm"}>
+                  <span className="text-sm">
+                    Enter {BRAND_NAME}
+                  </span>
+                  <FileInput className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+              <UserButton />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ components/spinner.tsx
@@ -0,0 +1,24 @@
+import { Loader } from "lucide-react";
+
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "@/lib/utils";
+
+const spinnerVariants = cva("animate-spin", {
+  variants: {
+    size: {
+      default: "w-4 h-4",
+      sm: "w-2 h-2",
+      lg: "w-6 h-6",
+      xl: "w-10 h-10",
+    },
+  },
+  defaultVariants: {
+    size: "default",
+  },
+});
+
+export interface SpinnerProps extends VariantProps<typeof spinnerVariants> {}
+
+export const Spinner = ({ size }: SpinnerProps) => {
+  return <Loader className={cn(spinnerVariants({ size }), "text-muted-foreground")} />;
+};
