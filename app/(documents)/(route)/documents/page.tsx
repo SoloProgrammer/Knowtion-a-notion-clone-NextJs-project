04e76c04459a3a703e2db41a9594023dac2802b2
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-13 13:47:06 +0530

    Add user action item in the sidebar, updated douments page

--- app/(documents)/(route)/documents/page.tsx
+++ app/(documents)/(route)/documents/page.tsx
@@ -1,10 +1,36 @@
+"use client";
+
+import Image from "next/image";
 import { SideBarMenu } from "../../_components/sidebar/side-bar-menu";
 
+import { useUser } from "@clerk/clerk-react";
+
+import { BRAND_NAME } from "@/app/constants";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+  const { user } = useUser();
+
   return (
-    <div className="p-3 flex items-start gap-2 flex-col md:flex-row md:items-center">
-      <SideBarMenu />
-      <p>This is a restricted page to authenticate users only!</p>
+    <div className="flex relative items-start flex-col h-full">
+      <SideBarMenu className="absolute top-2 left-2" />
+      <div className="flex-grow w-full flex items-center justify-center flex-col">
+        <div className="relative w-[350px] h-[350px]">
+          <Image
+            fill
+            src={"/document-welcome.png"}
+            className="dark:invert object-contain"
+            alt="documents-welcome"
+          />
+        </div>
+        <p className="font-semibold">
+          Welcome to {user?.firstName}&apos;s {BRAND_NAME}
+        </p>
+        <Button className="mt-5" size={"sm"}>
+          <PlusCircle className="w-4 h-4 mr-2"/> Create a note
+        </Button>
+      </div>
     </div>
   );
 };
