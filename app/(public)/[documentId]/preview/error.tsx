7d3b146fc1d2135823ae72801ce51269fde8c158
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-18 02:57:39 +0530

    Make logo visible on preview pages on mobile screens

--- app/(public)/[documentId]/preview/error.tsx
+++ app/(public)/[documentId]/preview/error.tsx
@@ -12,12 +12,12 @@ import { Logo } from "@/app/(marketing)/_components/logo";
 const PreviewErrorPage = () => {
   return (
     <div className="flex h-full w-full justify-center items-center flex-col gap-y-4 px-3">
-      <Logo hideBrandName className="fixed z-10 bottom-5 right-5" />
+      <Logo hideBrandName className="fixed z-10 bottom-5 right-5 !inline-flex" />
       <h1 className="text-4xl md:text-5xl font-semibold text-center">
         There is nothing here to preview!
       </h1>
       <small className="text-sm text-muted-foreground text-center">
-        The document you are trying to view is not published yet
+        The document you are trying to view is&nbsp;not&nbsp;published&nbsp;yet
       </small>
       <Image
         width={300}
