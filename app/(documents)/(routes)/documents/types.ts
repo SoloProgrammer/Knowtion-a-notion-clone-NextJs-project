a5dd186f919f61dd17895e42b1da55f592ea0886
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-28 02:54:44 +0530

    Created some custom types

---
+++ app/(documents)/(routes)/documents/types.ts
@@ -0,0 +1,12 @@
+export type Collaborator = {
+  name: string;
+  email: string;
+  imgUrl: string;
+};
+
+export type User = {
+  id?: string;
+  name: string;
+  email: string;
+  imgUrl: string;
+};
