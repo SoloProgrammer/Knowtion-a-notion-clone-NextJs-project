00197562433b067bf4e5e8f036b636938c7860b9
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-28 02:48:01 +0530

    Return users with custom user type

--- actions/user.actions.ts
+++ actions/user.actions.ts
@@ -1,8 +1,21 @@
 "use server";
 
-import { clerkClient, User } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs/server";
+
+export type User = {
+  id?: string;
+  name: string;
+  email: string;
+  imgUrl: string;
+};
 
 export const useSearchUsers = async (query: string) => {
-  const { data: users } = await clerkClient().users.getUserList({ query });
+  const { data } = await clerkClient().users.getUserList({ query });
+  const users = data.map((user) => ({
+    id: user.id,
+    name: user.firstName,
+    email: user.emailAddresses[0].emailAddress,
+    imgUrl: user.imageUrl,
+  }));
   return JSON.parse(JSON.stringify(users)) as User[];
 };
