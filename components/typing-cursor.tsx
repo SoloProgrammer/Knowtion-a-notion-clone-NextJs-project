3e2ba5438e29f1f498f38dc4b5370f4c50b8c8cc
Pratham Shinde <107343791+SoloProgrammer@users.noreply.github.com>
2024-07-30 22:34:32 +0530

    Update typing-cursor.tsx

--- components/typing-cursor.tsx
+++ components/typing-cursor.tsx
@@ -10,8 +10,10 @@ export const TypingCursor = ({ color, text, className }: TypingCursorProps) => {
   return (
     <span
       data-text={text}
+      data-color={color}
+      style={{ background: color }}
       className={cn(
-        `h-[38px] w-[2px] bg-[${color}] inline-block relative before:absolute before:-top-4 before:rounded-tl before:rounded-tr before:rounded-br before:w-[max-content] before:px-1 before:h-fit before:bg-[${color}] before:left-0 before:content-[attr(data-text)] before:text-xs before:md:text-sm`,
+        `h-[38px] w-[2px] inline-block relative before:absolute before:-top-4 before:rounded-tl before:rounded-tr before:rounded-br before:w-[max-content] before:px-1 before:h-fit before:bg-inherit before:left-0 before:content-[attr(data-text)] before:text-xs before:md:text-sm`,
         className
       )}
     />
