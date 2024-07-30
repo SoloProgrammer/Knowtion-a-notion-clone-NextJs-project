22f3e58b60215fa2a0c280b349e2f258cc5f23b9
SoloProgrammer <prathamshinde987@gmail.com>
2024-07-30 22:08:54 +0530

    Added hero scroll container section inside marketing page

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -24,8 +24,8 @@ export const Navbar = () => {
   return (
     <div
       className={cn(
-        "px-4 md:px-6 py-3 md:py-4 fixed top-0 left-0 w-full transition-colors flex justify-between items-center",
-        isScrolled && "shadow-sm bg-white dark:bg-neutral-900 border-b"
+        "px-4 md:px-6 py-3 md:py-4 fixed top-0 left-0 w-full  backdrop-blur-md transition-colors flex justify-between items-center z-[999]",
+        isScrolled && "shadow-sm bg-white bg-transparent border-b"
       )}
     >
       <Logo className="flex" hideBrandName={false} />
