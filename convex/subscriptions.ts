6021537917861f8c3e361145844a9ab995dab671
SoloProgrammer <prathamshinde987@gmail.com>
2024-08-13 00:06:21 +0530

    Created convex suscription apis

---
+++ convex/subscriptions.ts
@@ -0,0 +1,60 @@
+import { ConvexError, v } from "convex/values";
+
+import { mutation, query } from "./_generated/server";
+import { SubscriptionDto } from "./schema";
+
+export const create = mutation({
+  args: {
+    plan: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Not authenticated");
+    }
+
+    const newSubscriptionId = await ctx.db.insert("subscriptions", {
+      plan: args.plan,
+      buyerId: identity.subject,
+    });
+
+    return newSubscriptionId;
+  },
+});
+
+export const update = mutation({
+  args: { ...SubscriptionDto, id: v.id("subscriptions") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Not authenticated");
+    }
+
+    const { id, ...rest } = args;
+    await ctx.db.patch(id, rest);
+
+    return "Subscription updated";
+  },
+});
+
+export const get = query({
+  args: {},
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError("Not authenticated");
+    }
+
+    const subscription = (
+      await ctx.db
+        .query("subscriptions")
+        .withIndex("by_buyer", (q) => q.eq("buyerId", identity.subject))
+        .collect()
+    )[0];
+
+    return subscription;
+  },
+});
